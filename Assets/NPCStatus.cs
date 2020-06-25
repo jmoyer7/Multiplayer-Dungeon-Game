217c078582b19377b357234871a8f2a163c58b27
jmoyer7 <60228623+jmoyer7@users.noreply.github.com>
2020-06-25 12:58:43 -0400

    Beginning of attacking UI
    Pop up button next to enemy, activates when enemy is clicked

---
+++ Assets/NPCStatus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCStatus : MonoBehaviour
+{
+    public int health = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "enemy")
+                { this.transform.GetChild(0).gameObject.SetActive(true); }
+            }
+        }
+    }
+}
---
+++ Assets/UIButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIButton : MonoBehaviour
+{
+
+    public Sprite regular;
+    public Sprite mouseOver;
+    public Sprite mouseClicked;
+    public TextMeshPro buttonText;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        print("ATTACK");
+        //get parent of button
+        //remove health
+    }
+
+    private void OnMouseEnter()
+    {
+
+    }
+
+    private void OnMouseExit()
+    {
+
+    }
+
+    private void OnMouseUpAsButton()
+    {
+
+    }
+}
