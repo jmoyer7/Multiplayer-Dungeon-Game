5b2ba97b92637a21f89a3133ecee6f55a1d2788b
jmoyer7 <60228623+jmoyer7@users.noreply.github.com>
2020-06-30 18:12:30 -0400

    Working drag inventory / snap to slot

---
+++ Assets/ItemDragHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour,IPointerDownHandler,IBeginDragHandler,IEndDragHandler,IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    [SerializeField] private Canvas canvas;
+
+    
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+  
+    public void OnPointerDown(PointerEventData eventData)
+    {
+       
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print("END DRAG");
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta/canvas.scaleFactor;
+    }
+}
